b36f7625441e01559014d8b09e89d97952851634
Aotae <47459782+Aotae@users.noreply.github.com>
2019-07-15 13:15:06 -0700

    Add files via upload

---
+++ GridMap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMap : MonoBehaviour
+{
+    private GameObject[,] Map;
+    private GameObject[,] NewMap;
+    public GameObject Ground;
+    public GameObject MapHold;
+    public int Xmin;
+    public int Xmax;
+    public int Ymin;
+    public int Ymax;
+    public int X;
+    public int Y;
+    //Map Size//
+    void Start()
+    {
+        float xscale = Ground.transform.localScale.x;
+        float yscale = Ground.transform.localScale.y;
+        System.Random rand = new System.Random();
+        X = rand.Next(Xmin, Xmax);
+        Y = rand.Next(Ymin, Ymax);
+        Map = new GameObject[X, Y];
+        NewMap = new GameObject[X, Y];
+        for (int x = 0; x < X; x++)
+        {
+            for (int y = 0; y < Y; y++)
+            {
+                Ground.transform.position = new Vector3(x * xscale,0, y * yscale);
+                Map[x, y] = Ground;
+                NewMap[x, y] = Instantiate(Map[x, y]);
+                NewMap[x, y].transform.parent = MapHold.transform;
+            }
+        }
+    }
+}
+
+
