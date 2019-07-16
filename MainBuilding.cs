147d7ab41038d59a687a26d539193cf8b5f1f8f5
Chelsea Li <chelseali2001@gmail.com>
2019-07-16 09:38:30 -0700

    Add files via upload

---
+++ MainBuilding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainBuilding : MonoBehaviour {
+    public int level = 1, coins = 500, xp = 0;
+    public TextMesh t;
+
+    public void Start() {
+        GameObject text = new GameObject();
+        t = text.AddComponent<TextMesh>();
+    }
+
+
+    public void OnMouseDown() {
+        t.text = "Level: " + level.ToString() + "\nCoins: " + coins.ToString() + "\nXP: " + xp.ToString() + "/" + (level * level * 10).ToString();
+        //t.fontSize = 30;
+        t.transform.localPosition += new Vector3(-15f, 10f, -5);
+        //print("Level: " + level + "\nCoins: " + coins + "\nXP: " + xp + "/" + (level * level * 10));
+    }
+}
